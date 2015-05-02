08e290bce0d5861fa78459280d07fcb31669ac65
pramoth <pramoth@geniustree.co.th>
2015-05-02 20:22:05 +0700

    -add test with thread that only capture time of calculation.

--- src/main/java/th/co/geniustree/internship/SumWithThread.java
+++ src/main/java/th/co/geniustree/internship/SumWithThread.java
@@ -44,8 +44,8 @@ public class SumWithThread {
 
     private static void testWithSingleThread(int[] array) {
         long startTime = System.nanoTime();
-        int sum = 0;
         for (int t = 0; t < THREAD_COUNT; t++) {
+            int sum = 0;
             for (int i = 0; i < array.length; i++) {
                 sum += array[i];
             }
